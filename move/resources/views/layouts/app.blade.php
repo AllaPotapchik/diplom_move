6b0b6e1a48396250b98de9923b799f994afda691
allmeste <apotapchik62@gmail.com>
2024-04-04 15:09:24 +0300

    add adaptive footer

--- move/resources/views/layouts/app.blade.php
+++ move/resources/views/layouts/app.blade.php
@@ -50,7 +50,8 @@
             <a href="{{ url('/') }}"> <img class="account_icon" src="{{asset('images/account_icon.png')}}"></a>
 
             <div class="menu_burger">
-                <a href="{{ url('/') }}"> <img class="account_icon_burger" src="{{asset('images/account_icon.png')}}"></a>
+                <a href="{{ url('/') }}"> <img class="account_icon_burger"
+                                               src="{{asset('images/account_icon.png')}}"></a>
                 <div class="menu_burger__field" id="menu_burger">
                     <span class="bar"></span>
                     <span class="bar"></span>
@@ -62,99 +63,67 @@
         <div class="popup" id="popup"></div>
     </header>
 
-        {{--  <div class="container header__wrap">
-             <div class="logo__wrap">
-                 <a href="{{ url('/') }}"><img class="logo" src="{{asset('images/logo.png')}}"></a></div>
-             <nav>
-                 <ul class="menu">
-                     <li>
-                         <a href="#">Расписание</a>
-                     </li>
-                     <li>
-                         <a href="#">Направления</a>
-                     </li>
-                     <li>
-                         <a href="#">Преподаватели</a>
-                     </li>
-                     <li>
-                         <a href="#">Тарифы</a>
-                     </li>
-                     <li>
-                         <a href="#">О нас</a>
-                     </li>
-                     <li>
-                         <a href="#">Контакты</a>
-                     </li>
-                 </ul>
-                 <div class="burger">
-                     <div class="line"></div>
-                     <div class="line"></div>
-                     <div class="line"></div>
-                 </div>
-             </nav>
-                 <a href="{{ url('/') }}"> <img class="account_icon" src="{{asset('images/account_icon.png')}}"></a>
-         </div> --}}
-     {{-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-          <div class="container">
-              <a class="navbar-brand" href="{{ url('/') }}">
-                  {{ config('app.name', 'Laravel') }}
-              </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <!-- Left Side Of Navbar -->
-                  <ul class="navbar-nav me-auto">
-
-                  </ul>
-
-                  <!-- Right Side Of Navbar -->
-                  <ul class="navbar-nav ms-auto">
-                      <!-- Authentication Links -->
-                      @guest
-                          @if (Route::has('login'))
-                              <li class="nav-item">
-                                  <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                              </li>
-                          @endif
-
-                          @if (Route::has('register'))
-                              <li class="nav-item">
-                                  <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                              </li>
-                          @endif
-                      @else
-                          <li class="nav-item dropdown">
-                              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                  {{ Auth::user()->name }}
-                              </a>
-
-                              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                  <a class="dropdown-item" href="{{ route('logout') }}"
-                                     onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
-                                      {{ __('Logout') }}
-                                  </a>
-
-                                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                      @csrf
-                                  </form>
-                              </div>
-                          </li>
-                      @endguest
-                  </ul>
-              </div>
-          </div>
-      </nav>--}}
-
     <main class="py-4">
         @yield('content')
     </main>
+
+    <footer>
+        <div class="container footer__wrap">
+            <ul class="menu" id="menu">
+                <li>
+                    <a href="#">Расписание</a>
+                </li>
+                <li>
+                    <a href="#">Направления</a>
+                </li>
+                <li>
+                    <a href="#">Преподаватели</a>
+                </li>
+                <li>
+                    <a href="#">Тарифы</a>
+                </li>
+                <li>
+                    <a href="#">О нас</a>
+                </li>
+                <li>
+                    <a href="#">Контакты</a>
+                </li>
+            </ul>
+<div class="info__wrap">
+            <div>
+                <a href="{{ url('/') }}"> <img class="socials" src="{{asset('images/inst.png')}}"></a>
+                <a href="{{ url('/') }}"> <img class="socials" src="{{asset('images/telegram.png')}}"></a>
+                <a href="{{ url('/') }}"> <img class="socials" src="{{asset('images/tiktok.png')}}"></a>
+            </div>
+            <div>
+                <p>
+                    <b>Адресс</b>
+                    <br>
+                    <br>
+                    г. Минск
+                    ул. Скрыганова д.16
+                </p>
+            </div>
+            <div><p><b>Телефон</b>
+                    <br>
+                    <br>
+                    МТС <a href="tel:+375 33 6993616">+375 33 6993616</a>
+                    <br>
+                    А1 <a href="tel:+375 29 6993616">+375 29 6993616</a>
+                </p>
+            </div>
+            <div>
+                <p><b>Email</b>
+                    <br>
+                    <br>
+                    <a href="mailto:movedance@gmail.com">movedance@gmail.com</a>
+                </p>
+            </div>
+        </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script type="text/javascript" src="https://unpkg.com/imask"></script>
+    </footer>
 </div>
-<footer>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script type="text/javascript" src="https://unpkg.com/imask"></script>
-</footer>
 </body>
 </html>
